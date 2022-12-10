fd2dcf24b02f891b6f39471ac3629c61af4a7589
anmutu <anmutu@hotmail.com>
2022-12-10 08:38:33 +0800

    视频爬楼梯

--- np/01-070-climbing-stairs/climbing-staris.go
+++ np/01-070-climbing-stairs/climbing-staris.go
@@ -2,8 +2,8 @@ package main
 
 import "fmt"
 //1,2,3,4,5
-//1,2,3,5,8
-
+//1,2,    3,    5,      8
+//1,2    1+2   2+3     3+5
 func main() {
 	fmt.Println(climbStairs(1))
 	fmt.Println(climbStairs(2))
@@ -17,13 +17,13 @@ func climbStairs(n int) int {
 	if n==2{
 		return 2
 	}
-	//1.定义容器
+	//1.定义容器。
 	dp:=make([]int,n)
-	//2.初始化值。
+	//2.找到初始值。
 	dp[0],dp[1]=1,2
 	//3.根据状态转移方程写相关代码。
 	for i:=2;i<n;i++{
-		dp[i]=dp[i-1]+dp[i-2]
+		dp[i]=dp[i-2]+dp[i-1]
 	}
 	return dp[n-1]
 }
