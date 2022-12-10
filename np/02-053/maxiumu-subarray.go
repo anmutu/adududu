40b5e3ab505a3d3312870315ca1af6a65e3eb379
anmutu <anmutu@hotmail.com>
2022-12-10 15:10:25 +0800

    053视频讲解最大子数和

--- np/02-053/maxiumu-subarray.go
+++ np/02-053/maxiumu-subarray.go
@@ -2,42 +2,32 @@ package main
 
 import "fmt"
 
-//-2,1,-3,4,-1,2,1,-5,4
-
-//难点：找出关系。
-//创建一个数组，里面装的是到它这个位置的最大值是多少。
-//1.对于第一个元素来说就是它自己了。
-//2.对于第二个元素来说就是[第一个元素+自己]和[自己]比较大的值了。
-//-2,            1,           -3,              4,        -1,              2,      1,     -5,    4
-//-2,      -2+1，1,=>1    1+(-3),-3=>-2     -2+4,4=>4  4+(-1),-1=>3        5      6      1     5
+//-2,         1,           -3,                4,            -1,            2,             1,          -5,              4
+//-2,     -2+1,1=>1    1+(-3),-3=> -2    4+(-2),4=>4    -1+(4),-1=>3     3+2,2=>5      5+1,1=>6   6+(-5),-5=>1      1+4，4=>5
+//难点：找关系。
+//第一个位置，就是取第一个位置的值。
+//从第二个数开始就是取[自己]和[自己与其前面的数相加的结果]中的较大值。
 func main(){
 	nums:=[]int{-2,1,-3,4,-1,2,1,-5,4}
 	fmt.Println(maxSubarray(nums))
-	nums1:=[]int{1}//返回值要定义为nums[0],不然是会不通过的。
+	nums1:=[]int{1}
 	fmt.Println(maxSubarray(nums1))
 }
 
 func maxSubarray(nums []int)int{
 	var res=nums[0]
-	
-	//1.定义容器
-	n:=len(nums)
+	//1.定义容器。里面的每个元素是到这个元素的最大的子数和的值。
+	var n=len(nums)
 	dp:=make([]int,n)
-
-	//2.找到初始值
+	//2.找到初始值。
 	dp[0]=nums[0]
-	// dp[1]=max(nums[0],nums[1])
-
-	//3.状态转移方程
+	//3，根据状态转移方程写相关代码。
 	for i:=1;i<n;i++{
-		dp[i]=max(nums[i]+dp[i-1],nums[i])
-		if res<dp[i]{
+		dp[i]=max(nums[i],nums[i]+dp[i-1])
+		if dp[i]>res{
 			res=dp[i]
-			fmt.Println(res)
 		}
-		fmt.Println(dp)
 	}
-
 	return res
 }
 
@@ -48,3 +38,4 @@ func max(x,y int)int{
 		return y
 	}
 }
+
