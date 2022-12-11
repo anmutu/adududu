b157b702a8765c640183003d4b8a0d6c3874563b
anmutu <anmutu@hotmail.com>
2022-12-11 16:13:49 +0800

    062不同路径视频讲解

--- np/03-062/unique-paths.go
+++ np/03-062/unique-paths.go
@@ -10,22 +10,21 @@ func main(){
 }
 
 func uniquePaths(m,n int)int{
-	//1.定义容器。先行再列。
+	//1.定义容器。
 	dp:=make([][]int,m)
-	fmt.Println(dp)
 	for i:=0;i<m;i++{
 		dp[i]=make([]int,n)
 	}
 	fmt.Println(dp)
 
-	//2.找初始值。
-	//3.根据状态转移公式写代码。
 	for i:=0;i<m;i++{
 		for j:=0;j<n;j++{
+			//2.找到初始值。
 			if i==0||j==0{
-			  dp[i][j]=1
-			  continue
+				dp[i][j]=1
+				continue
 			}
+			//3.根据状态转移方程写代码。
 			dp[i][j]=dp[i-1][j]+dp[i][j-1]
 		}
 	}
