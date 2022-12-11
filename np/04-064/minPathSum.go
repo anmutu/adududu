aba3993511df5f079d910e2efa09f8571fef4fa8
anmutu <anmutu@hotmail.com>
2022-12-11 18:20:26 +0800

    064

---
+++ np/04-064/minPathSum.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+func main(){
+	var grid =[][]int{
+		{1,3,1},
+		{1,5,1},
+		{4,2,1},
+	}
+	fmt.Println(minPathSum(grid))
+}
+
+func minPathSum(grid [][]int) int {
+	fmt.Println(grid)
+	//1.定义容器。
+	m:=len(grid)//行
+	n:=len(grid[0])//列
+
+	//2.找到初始值。
+	//第一列。
+	for i:=1;i<m;i++{
+		grid[i][0]+=grid[i-1][0]
+	}
+	//第一行
+	for j:=1;j<n;j++{
+		grid[0][j]+=grid[0][j-1]
+	}
+	fmt.Println(grid)
+
+	//3.根据状态转移方程写代码。
+	for i:=1;i<m;i++{
+		for j:=1;j<n;j++{
+			grid[i][j]+=min(grid[i-1][j],grid[i][j-1])
+		}
+	}
+	fmt.Println(grid)
+	return grid[m-1][n-1]
+}
+
+func min(x,y int)int{
+	if x<y{
+		return x
+	}else{
+		return y
+	}
+}
