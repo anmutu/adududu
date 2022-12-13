470c0bda3e2503515b8cb3b35b48ba85b43b28b9
anmutu <anmutu@hotmail.com>
2022-12-14 07:19:18 +0800

    198 你是一个会dp的小偷

---
+++ np/05-198/rob.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+func main(){
+	var nums=[]int{2,7,9,3,1}
+	fmt.Println(rob(nums))
+	var nums1=[]int{2,7}
+	fmt.Println(rob(nums1))
+}
+
+// 2,7,       9,         3,              1
+// 2,7,    9+2,7=>11   3+2,11=>11     1+11,11=>12
+//dp[i]=max(nums[i]+dp[i-2],dp[i-1])
+func rob(nums []int) int {
+	if len(nums)==1{
+		return nums[0]
+	}
+	var res=max(nums[0],nums[1])
+	
+	//1.定义容器。
+	n:=len(nums)
+	dp:=make([]int,n)
+
+	//2.找到初始值。
+	dp[0],dp[1]=nums[0],max(nums[0],nums[1])
+
+	//3.根据状态转移方程写代码。
+	for i:=2;i<n;i++{
+		dp[i]=max(nums[i]+dp[i-2],dp[i-1])
+		if dp[i]>res{
+			res=dp[i]
+		}
+	}
+	fmt.Println(dp)
+
+	return res
+}
+
+func max(x,y int)int{
+	if x>y{
+		return x
+	}else{
+		return y
+	}
+}
