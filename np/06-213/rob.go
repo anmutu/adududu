3f39ce97e9a647199e69296a15b596528e51f497
anmutu <anmutu@hotmail.com>
2022-12-30 00:05:55 +0800

    213打家劫舍2视频讲解

--- np/06-213/rob.go
+++ np/06-213/rob.go
@@ -7,14 +7,13 @@ func main(){
 	fmt.Println(rob(nums))
 }
 
-// 2,7,       9,         3,              1
-// 2,7,    9+2,7=>11   3+2,11=>11     1+11,11=>12
 //dp[i]=max(nums[i]+dp[i-2],dp[i-1])
 func rob(nums []int) int {
 	res1:=rob198(nums[0:len(nums)-1])
 	res2:=rob198(nums[1:])
 	return max(res1,res2)
 }
+
 
 func rob198(nums []int)int{
 	fmt.Println(nums)
@@ -24,14 +23,15 @@ func rob198(nums []int)int{
 	if len(nums)==2{
 		return max(nums[0],nums[1])
 	}
-
-	var res=nums[0]
+	res:=nums[0]
 	//1.定义容器。
 	n:=len(nums)
 	dp:=make([]int,n)
-	//2.找出初始值。
+
+	//2.找到初始值.
 	dp[0],dp[1]=nums[0],max(nums[0],nums[1])
-	//3.根据状态转移方程写代码。
+
+	//3.根据状态转移方程写相关代码。
 	for i:=2;i<n;i++{
 		dp[i]=max(nums[i]+dp[i-2],dp[i-1])
 		if dp[i]>res{
@@ -48,4 +48,3 @@ func max(x,y int)int{
 		return y
 	}
 }
-
