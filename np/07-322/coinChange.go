83d595832ddbd13f371c7231e825b1c985654186
anmutu <anmutu@hotmail.com>
2023-01-03 21:19:21 +0800

    322视频录制

--- np/07-322/coinChange.go
+++ np/07-322/coinChange.go
@@ -10,31 +10,31 @@ func main(){
 	// coins1 :=[]int{2}
 	// amount1:=3
 	// fmt.Println(coinChange(coins1,amount1))
-
 }
 
 func coinChange(coins []int, amount int) int {
 	//1.定义容器。
-	dp := make([]int, amount+1)
-	for i := 1; i < len(dp); i++ {
-		dp[i] = amount + 1
+	dp:=make([]int,amount+1)
+	for i:=0;i<len(dp);i++{
+		dp[i]=amount+1
 	}
 
 	//2.找出初始值。
-	dp[0] = 0
+	dp[0]=0
 
-	//3.写相关代码。
-	for i := 1; i <= amount; i++ {
-		fmt.Println("++++++++++++++++++++++++++++++++++++++++++++++++++++start=>i=>",i)
-		for j := 0; j < len(coins); j++ {
-			if coins[j] <= i {
-				fmt.Println("i=>",i,"dp[i]=>",dp[i],"dp[i-coins[j]]+1=>",dp[i-coins[j]]+1)
-				dp[i] = min(dp[i], dp[i-coins[j]]+1)
+	//3.根据状态转移方程写代码。
+	for i:=1;i<=amount;i++{
+		fmt.Println("*************************start******",i)
+		for j:=0;j<len(coins);j++{
+			if coins[j]<=i{
+				fmt.Println("dp[i-coins[j]]+1=>",dp[i-coins[j]]+1)
+				dp[i]=min(dp[i],dp[i-coins[j]]+1)
 			}
 		}
-		fmt.Println("+++++++++++++++++++++++++++++++++++++++++++++++++++++end=>i=>",i)
+		fmt.Println("*************************end*******",i)
 	}
-	if dp[amount] > amount {
+	fmt.Println(dp)
+	if dp[amount]>amount{
 		return -1
 	}
 	return dp[amount]
@@ -43,7 +43,6 @@ func coinChange(coins []int, amount int) int {
 func min(x,y int)int{
 	if x<y{
 		return x
-	}else{
-		return y
 	}
+	return y
 }
