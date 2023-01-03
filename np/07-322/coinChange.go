f2d1bbde9e4017d60ede6a04c36ccb0efd628435
anmutu <anmutu@hotmail.com>
2023-01-03 14:06:39 +0800

    322

--- np/07-322/coinChange.go
+++ np/07-322/coinChange.go
@@ -16,19 +16,19 @@ func main(){
 func coinChange(coins []int, amount int) int {
 	//1.定义容器。
 	dp := make([]int, amount+1)
-
-	//2.找出初始值。
-	dp[0] = 0
 	for i := 1; i < len(dp); i++ {
 		dp[i] = amount + 1
 	}
+
+	//2.找出初始值。
+	dp[0] = 0
 
 	//3.写相关代码。
 	for i := 1; i <= amount; i++ {
 		fmt.Println("++++++++++++++++++++++++++++++++++++++++++++++++++++start=>i=>",i)
 		for j := 0; j < len(coins); j++ {
 			if coins[j] <= i {
-				fmt.Println("dp[i]=>",dp[i],"dp[i-coins[j]]+1=>",dp[i-coins[j]]+1)
+				fmt.Println("i=>",i,"dp[i]=>",dp[i],"dp[i-coins[j]]+1=>",dp[i-coins[j]]+1)
 				dp[i] = min(dp[i], dp[i-coins[j]]+1)
 			}
 		}
